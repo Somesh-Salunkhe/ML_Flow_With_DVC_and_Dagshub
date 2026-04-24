92ef9ccdf5de124d2faac18ea7157b66ac0f8be3
Somesh Salunkhe <salunkhesomesh1602@gmail.com>
2026-04-24 18:47:11 +0200

    Train.py

---
+++ src/train.py
@@ -0,0 +1,109 @@
+## Trainer
+
+# Imports
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import yaml
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from mlflow.models import infer_signature
+import os
+from sklearn.model_selection import train_test_split, GridSearchCV
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import mlflow
+
+# Loading environment variables
+load_dotenv()
+os.environ['MLFLOW_TRACKING_URI'] = os.getenv("MLFLOW_TRACKING_URI")
+os.environ["MLFLOW_TRACKING_USERNAME"] = os.getenv("MLFLOW_TRACKING_USERNAME")
+os.environ["MLFLOW_TRACKING_PASSWORD"] = os.getenv("MLFLOW_TRACKING_PASSWORD")
+
+
+# Function for hyperparamete tuning
+def hyperparameter_tuning(X_train,y_train, param_grid):
+    # Classifier
+    rf = RandomForestClassifier()
+
+    # Grid search
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+    grid_search.fit(X_train, y_train)
+    return grid_search
+
+# Load training config from config.yaml
+params = yaml.safe_load(open("config.yaml"))["train"]
+
+
+# Model trainer and ML flow logging function
+def trainer(data_path,model_path,random_state):
+    # Read processed data
+    data = pd.read_csv(data_path)
+
+    # Splitting data
+    X = data.drop(columns=["Outcome"])
+    y = data['Outcome']
+
+    # Setting mlflow tracking uri
+    mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
+
+    # Start ML Flow run
+    with mlflow.start_run():
+        # Split data into train and test datasets
+        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=random_state)
+
+        # Signature for input and ouput schema
+        signature = infer_signature(X_train, y_train)
+
+        # Define hyperparameter grid
+        param_grid = {
+            'n_estimators' : [100,200],
+            'max_depth' : [5,10,None],
+            'min_samples_split' : [2,5],
+            'min_samples_leaf' : [1,2]
+        }
+
+        # Perform hyperparameter tuning
+        grid_search = hyperparameter_tuning(X_train, y_train, param_grid)
+
+        # Best model
+        best_model = grid_search.best_estimator_
+
+        # Prediction
+        y_pred = best_model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"Accuracy: {accuracy}")
+
+        # Log metrics
+        mlflow.log_metric('accuracy', accuracy)
+        mlflow.log_param("best_n_estimators", grid_search.best_params_['n_estimators'])
+        mlflow.log_param("best_max_depth", grid_search.best_params_['max_depth'])
+        mlflow.log_param("best_samples_split", grid_search.best_params_['min_samples_split'])
+        mlflow.log_param("best_samples_leaf", grid_search.best_params_['min_samples_leaf'])
+
+        # Log confusion matrix and classification report
+        cm = confusion_matrix(y_test, y_pred)
+        cr = classification_report(y_test, y_pred)
+
+        mlflow.log_text(str(cm), "confusion_matrix.txt")
+        mlflow.log_text(str(cr), "classification_report.txt")
+
+        # Verify tracking uri for logging of model
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != 'file':
+            mlflow.sklearn.log_model(best_model, "model", registered_model_name='Best model')
+        else:
+            mlflow.sklearn.log_model(best_model, "model", signature=signature)
+
+        # Create directory to save model
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+
+        # Save model
+        filename = model_path
+        pickle.dump(best_model, open(filename, 'wb'))
+
+        print(f"Model saved to {model_path}")
+
+
+if __name__ == "__main__":
+    trainer(params['data'], params['model'], params['random_state'])
